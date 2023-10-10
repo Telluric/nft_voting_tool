105c40894ea19c00a58bd130938342552358e8bb
Michael Feher <michael.feher@algorand.foundation>
2023-10-10 13:22:59 -0500

    chore: eslint fix and update eslint

--- src/xgov-dapp/src/features/vote-creation/VoteCreation.stories.tsx
+++ src/xgov-dapp/src/features/vote-creation/VoteCreation.stories.tsx
@@ -3,7 +3,11 @@ import VoteCreationPage from './RoundInfo'
 import Root from '../../../.storybook/Root'
 export default {
   title: 'Admin',
-  render: () => <Root><VoteCreationPage /></Root>,
+  render: () => (
+    <Root>
+      <VoteCreationPage />
+    </Root>
+  ),
 }
 
 export const CreateRound = {}
--- src/xgov-dapp/src/features/wallet/Wallet.stories.tsx
+++ src/xgov-dapp/src/features/wallet/Wallet.stories.tsx
@@ -7,18 +7,17 @@ export default {
   render: () => {
     const ref = useRef(false)
     const showConnectedWalletModal = useShowConnectWalletModal()
-    const setShowConnectedWalletModal =  useSetShowConnectWalletModal()
+    const setShowConnectedWalletModal = useSetShowConnectWalletModal()
     setShowConnectedWalletModal(true)
     useEffect(() => {
-      console.log({ref, showConnectedWalletModal})
-      if(!ref.current && showConnectedWalletModal) {
+      console.log({ ref, showConnectedWalletModal })
+      if (!ref.current && showConnectedWalletModal) {
         ref.current = true
       }
 
-      if(!showConnectedWalletModal && ref.current){
-          alert('Wallet Connect Closed')
+      if (!showConnectedWalletModal && ref.current) {
+        alert('Wallet Connect Closed')
       }
-
     }, [showConnectedWalletModal])
     return <ConnectWalletModal />
   },
